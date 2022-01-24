b7cf94a7821029ba13e3cf83d3f3c42f0b8a9494
ebru <ebrucetinb@gmail.com>
2022-01-24 09:39:35 -0600

    doing final work on hello feature

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -3,7 +3,12 @@ package Hello;
 public class Hello {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+
+        System.out.println("B25 Learning Git and GitHub");
+
+
     }
 
 }
